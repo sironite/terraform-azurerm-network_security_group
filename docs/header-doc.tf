68a82b50d6e984ecabd2c57418abefe328edef96
TheIronRock95 <58424695+TheIronRock95@users.noreply.github.com>
2023-01-24 21:12:23 +0100

    fix: update standards

--- docs/header-doc.tf
+++ docs/header-doc.tf
@@ -1,6 +1,6 @@
 /**
  *  # Network Security Group
- * [![Changelog](https://img.shields.io/badge/changelog-release-green.svg)](CHANGELOG.md) [![Notice](https://img.shields.io/badge/notice-copyright-yellow.svg)](NOTICE) [![Apache V2 License](https://img.shields.io/badge/license-Apache%20V2-orange.svg)](LICENSE) [![TF Registry](https://img.shields.io/badge/terraform-registry-blue.svg)](https://registry.terraform.io/)
+ * [![Changelog](https://img.shields.io/badge/changelog-release-green.svg)](https://github.com/sironite/terraform-azurerm-network_security_group/releases) [![Notice](https://img.shields.io/badge/notice-copyright-yellow.svg)](NOTICE) [![Apache V2 License](https://img.shields.io/badge/license-Apache%20V2-orange.svg)](LICENSE) [![TF Registry](https://img.shields.io/badge/terraform-registry-blue.svg)](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/network_security_group)
  *
  * 
  *
